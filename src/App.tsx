c67b99c4274e6843d3ffed88da0f8d7daa269919
JonMarsh Anderson <jonmarshanderson@gmail.com>
2025-07-08 08:09:22 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-green-800 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-extrabold text-yellow-400 p-8">
+        TAILWIND IS HERE!
+      </h1>
+      <p className="text-xl text-white bg-black/60 rounded px-4 py-2 mt-4">
+        If you see yellow text on green, Tailwind is working!
+      </p>
+    </div>
+  );
+}
+export default App;
